446f62a5d1ea79d0c6487f633a034d0f8c1f34f8
jeaust8 <joostvanaartsen@gmail.com>
2021-04-29 10:30:09 +0200

    Update 28-4-2021:
    StartProject

--- src/main/java/nl/joostvaartsen/GarageApp/GarageAppApplication.java
+++ src/main/java/nl/joostvaartsen/GarageApp/GarageAppApplication.java
@@ -1,10 +1,13 @@
 package nl.joostvaartsen.GarageApp;
 
+import nl.joostvaartsen.GarageApp.customer.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,15 @@ public class GarageAppApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Customer> hello() {
+		return List.of(new Customer(
+				1L,
+				"Henk",
+				"Janssen",
+				"henk@janssen.mail.nl",
+				
+				LocalDate.of(2000, Month.JANUARY, 5),
+				21);
 	}
 
 }
--- src/main/java/nl/joostvaartsen/GarageApp/customer/Customer.java
+++ src/main/java/nl/joostvaartsen/GarageApp/customer/Customer.java
@@ -1,18 +1,20 @@
 package nl.joostvaartsen.GarageApp.customer;
 
+import java.time.LocalDate;
+
 public class Customer {
     private Long id;
     private String name;
     private String surname;
     private String email;
     private String licensePlateNumber;
-    private int carAge;
+    private LocalDate carAge;
 
     public Customer() {
     }
 
 
-    public Customer(Long id, String name, String surname, String email, String licensePlateNumber, int carAge) {
+    public Customer(Long id, String name, String surname, String email, String licensePlateNumber, LocalDate carAge) {
         this.id = id;
         this.name = name;
         this.surname = surname;
@@ -21,12 +23,72 @@ public class Customer {
         this.carAge = carAge;
     }
 
-    public Customer(String name, String surname, String email, String licensePlateNumber, int carAge) {
+    public Customer(String name, String surname, String email, String licensePlateNumber, LocalDate carAge) {
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.licensePlateNumber = licensePlateNumber;
         this.carAge = carAge;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public void setLicensePlateNumber(String licensePlateNumber) {
+        this.licensePlateNumber = licensePlateNumber;
+    }
+
+    public LocalDate getCarAge() {
+        return carAge;
+    }
+
+    public void setCarAge(LocalDate carAge) {
+        this.carAge = carAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", licensePlateNumber='" + licensePlateNumber + '\'' +
+                ", carAge=" + carAge +
+                '}';
     }
 }
 
